3b8c6641c9082e6947eda8ac0f3fe586434749d9
mengke10086 <76813027+mengke10086@users.noreply.github.com>
2026-07-19 01:52:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,11 @@
 # 取消注释一个源
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加第三方 feed 源（small-package 包含 openclash/passwall/ssr-plus 等常用插件）
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >> feeds.conf.default
+# 高通410 平台必需：ModemManager、qcom-firmware 等
+echo 'src-git-full openstick https://github.com/lkiuyu/openstick-feeds.git' >> feeds.conf.default
+# 添加第三方 feed 源（small-package 包含passwall2等常用插件）
+echo 'src-git passwall_packages https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> feeds.conf.default
 # 添加第三方源，iStore 应用商店，编译时包名输入 luci-app-store
 echo 'src-git store https://github.com/linkease/istore.git;main' >> feeds.conf.default
 
